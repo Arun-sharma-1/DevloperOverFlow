f8f0e3dba6426878b6c710291da73f66448c9147
arunsharma <arunmadhopur750@gmail.com>
2024-05-16 00:12:55 +0530

    connected to db

--- app/(root)/ask-questions/page.tsx
+++ app/(root)/ask-questions/page.tsx
@@ -1,10 +1,10 @@
+import Question from "@/components/form/Question";
 import { UserButton } from "@clerk/nextjs";
 const page = () => {
   return (
     <div>
-      {/* <UserButton afterSignOutUrl="/" />
-       */}
-      Ask your questions here....
+      <h1 className="h1-bold text-dark100_light900">Ask a Question</h1>
+      <Question />
     </div>
   );
 };
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -10,7 +10,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <Navbar />
       <div className="flex ">
        <LeftSideBar />
-        <section className="flex min-h-screen flex=1 flex-col px-6 pb-6 pt-24 max-md:pb-14 sm:px-14 w-full ">
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-24 max-md:pb-14 sm:px-14 w-full ">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
         <RightSidebar />
